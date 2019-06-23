fc8b9fae4b238ba00e3d9809b46d1c97012c55dd
神谷啓明 <kamiyahiroaki@kamiyahiroakinoMacBook-ea.local>
2019-06-23 21:46:33 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock "~> 3.11.0"
 
 set :application, 'mercari-a'
-set :repo_url,  'git@github.com:hiroakikamiya/mercari.git'
+set :repo_url,  'git@github.com:hiroakikamiya/mercari-a.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
