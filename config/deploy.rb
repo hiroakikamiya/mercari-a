a8b0ce1dc1567edf80f26777d378ea1ab56d9247
神谷啓明 <kamiyahiroaki@kamiyahiroakinoMacBook-ea.local>
2019-06-23 21:34:00 +0900

    deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' 
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.shh/53team-a.pem']
+                  keys: ['~/.ssh/53team-a.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
