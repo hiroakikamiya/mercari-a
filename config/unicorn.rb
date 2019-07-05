d879f57a9ad61faa5ad4d10397d58e77b85b0235
神谷啓明 <kamiyahiroaki@kamiyahiroakinoMacBook-ea.local>
2019-07-05 12:27:55 +0900

    unicorn.rb 編集

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,11 +2,11 @@ app_path = File.expand_path('../../../', __FILE__)
 
 worker_processes 1
 
-working_directory "#{app_path}/mercari-a/current"
-listen "#{app_path}/mercari-a/shared/tmp/sockets/unicorn.sock"
-pid "#{app_path}/mercari-a/shared/tmp/pids/unicorn.pid"
-stderr_path "#{app_path}/mercari-a/shared/log/unicorn.stderr.log"
-stdout_path "#{app_path}/mercari-a/shared/log/unicorn.stdout.log"
+working_directory "#{app_path}/current"
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
 timeout 60
 
