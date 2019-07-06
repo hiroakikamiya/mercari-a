88be9800ce0ec515f0f44c7ee4bb10449010265a
神谷啓明 <kamiyahiroaki@kamiyahiroakinoMacBook-ea.local>
2019-07-06 12:39:14 +0900

    アクション名変更に伴うルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,8 @@ Rails.application.routes.draw do
   namespace :users do
     resources :registrations do
       collection do
-        get 'step1'
-        get 'step2'
-        get 'step3'
-        get 'step4'
+        get 'basic_information'
+        get 'phone_number'
       end
     end
   end
