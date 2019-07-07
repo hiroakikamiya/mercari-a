257c558973471350772b4d39ed4f3e4d35e2701f
神谷啓明 <kamiyahiroaki@kamiyahiroakinoMacBook-ea.local>
2019-07-07 17:53:54 +0900

    ダミーデータ用seedsファイル作成

---
+++ db/seeds/delivery_cost.rb
@@ -0,0 +1,6 @@
+DeliveryCost.create(
+  [
+    {cost: '送料込み（出品者負担）'},
+    {cost: '着払い（購入者負担）'}
+  ]
+)
---
+++ db/seeds/delivery_date.rb
@@ -0,0 +1,7 @@
+DeliveryDate.create(
+  [
+    {date: '1〜2日で発送'},
+    {date: '2〜3日で発送'},
+    {date: '4〜7日で発送'}
+  ]
+)
