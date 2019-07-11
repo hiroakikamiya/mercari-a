28dedade241c28a700e79f104d7b6347e11de4e8
神谷啓明 <kamiyahiroaki@kamiyahiroakinoMacBook-ea.local>
2019-07-11 14:12:04 +0900

    ルーティング編集

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,17 @@ Rails.application.routes.draw do
       get 'search'
     end
   end
-  namespace :users do
-    resources :registrations do
-      collection do
-        get 'basic_information'
-        get 'phone_number'
-      end
-    end
+
+    devise_for :users, :controllers => {
+    :registrations => 'users/registrations'
+  }
+  
+  devise_scope :user do
+  get "user", :to => "users/registrations#index"
+  get "user/basic", :to => "users/registrations#basic"
+  get "user/phone_number", :to => "users/registrations#phone_number"
+  get "user/address", :to => "users/registrations#address"
+  get "user/card", :to => "users/registrations#card"
   end
+
 end
