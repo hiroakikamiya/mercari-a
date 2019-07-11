325ff4bf3bc662c7253aff4f75d53135b345e607
神谷啓明 <kamiyahiroaki@kamiyahiroakinoMacBook-ea.local>
2019-07-11 14:12:51 +0900

    スキーマファイル編集

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_06_122446) do
+ActiveRecord::Schema.define(version: 2019_07_09_090044) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -103,18 +103,21 @@ ActiveRecord::Schema.define(version: 2019_07_06_122446) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "email", null: false
+    t.string "email", default: "", null: false
     t.string "name_kanji", null: false
     t.string "name_kana", null: false
     t.string "nickname", null: false
     t.date "birthday", null: false
     t.string "phone_number", null: false
     t.text "comment"
-    t.string "crypted_password"
-    t.string "salt"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "images", "items"
