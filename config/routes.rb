87a6b91f86ac6ffd6943a6ad2ae97d49d87563a4
神谷啓明 <kamiyahiroaki@kamiyahiroakinoMacBook-ea.local>
2019-07-15 14:19:45 +0900

    routes.rb 編集

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,11 @@ Rails.application.routes.draw do
   get "user/card", :to => "users/registrations#card"
   end
 
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
 end
