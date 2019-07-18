ef86ceb0c34c089f78ea21255e5c630aee459481
神谷啓明 <kamiyahiroaki@kamiyahiroakinoMacBook-ea.local>
2019-07-18 19:23:17 +0900

    residenceバリデーション追加

--- app/models/residence.rb
+++ app/models/residence.rb
@@ -1,3 +1,8 @@
 class Residence < ApplicationRecord
   belongs_to :user, optional: true
+
+  validates :post_code, null: false
+  validates :prefecture, null: false
+  validates :city, null: false
+  validates :address, null: false
 end
